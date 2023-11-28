77ffbb87e8135d22bdbed45c8b87d2645ea4d2b7
Charbel Toumieh <125312312+Charbelto@users.noreply.github.com>
2023-11-29 00:45:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+from sympy import Matrix
 import numpy as np
-from collections import Counter
+from sympy.matrices.common import NonInvertibleMatrixError
+
 import numpy
 
 app = Flask(__name__)
@@ -28,33 +30,48 @@ def vigenere():
 @app.route('/hill', methods=['GET', 'POST'])
 def hill():
     if request.method == 'POST':
+        matrixSize = int(request.form['matrixSize'])
         
-        m11 = request.form['m11']
-        m12 = request.form['m12'] 
-        m13 = request.form['m13']
-        m21 = request.form['m21']
-        m22 = request.form['m22']
-        m23 = request.form['m23'] 
-        m31 = request.form['m31']
-        m32 = request.form['m32']
-        m33 = request.form['m33']
+        # Collect matrix values based on matrix size
+        key_matrix = []
+        for i in range(matrixSize):
+            row = []
+            for j in range(matrixSize):
+                element_name = f'm{i+1}{j+1}'
+                row.append(int(request.form[element_name]))
+            key_matrix.append(row)
 
-        key_matrix = [[int(m11), int(m12), int(m13)], 
-                      [int(m21), int(m22), int(m23)],
-                      [int(m31), int(m32), int(m33)]]
-                      
-        k = key_matrix
+        # Choose the appropriate function based on matrix size
+        # encrypt_func = encrypt_hill_3 if matrixSize == 3 else encrypt_hill_2
+        # decrypt_func = decrypt_hill_3 if matrixSize == 3 else decrypt_hill_2
+        if matrixSize == 2:
+            encrypt_func = encrypt_hill_2
+            decrypt_func = decrypt_hill_2
+        else:
+            encrypt_func = encrypt_hill_3
+            decrypt_func = decrypt_hill_3
+
         operation = request.form['operation']
         plaintext = request.form['plaintext'].lower()
-        #key_matrix = request.form['key_matrix']
 
-        if operation == "encrypt":
-            result = encrypt_hill(plaintext, k, alphabet_dict, dict2)
+        # if operation == "encrypt" and matrixSize==2:
+        #     result = encrypt_func(plaintext, key_matrix, alphabet_dict, dict2)
+        # else:
+        #     result = decrypt_func(plaintext, key_matrix, alphabet_dict, dict2)
+        if operation == "encrypt" and matrixSize==2:
+            result = encrypt_func(plaintext, key_matrix, alphabet_dict, dict2)
+        elif operation == "decrypt" and matrixSize==2:
+            result = decrypt_func(plaintext, key_matrix, alphabet_dict, dict2)
+        elif operation == "encrypt" and matrixSize==3:
+            result = encrypt_func(plaintext, key_matrix, alphabet_dict, dict2)
         else:
-            result = decrypt_hill(plaintext, k, alphabet_dict, dict2)
+            result = decrypt_func(plaintext, key_matrix, alphabet, dict2)
+
+
+
 
         return render_template('hill.html', result=result)
-		
+
     return render_template('hill.html')
 
 @app.route('/monoalphabetic', methods=['GET', 'POST'])  
@@ -130,7 +147,20 @@ dict2 = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i',
 # Hill cipher helpers                 
 alphabet_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9,'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18,'t': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
 dict22 = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# Use a list instead of a dictionary for alphabet
+alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+def decrypt_hill_3(plaintext, k, alphabet_list, dict2):
+    plaintext = check_word_3(plaintext)
+    
+    # Check if plaintext contains only alphabetical characters
+    if not plaintext.isalpha():
+        return "Invalid input. Please enter a valid plaintext."
+
+    ciphertext_numbers = [alphabet_list.index(char) for char in plaintext.upper()]
+    
+    # Rest of your decryption logic...
 
 
 # Monoalphabetic cipher helpers
@@ -205,9 +235,6 @@ def mod5(a):
 
 
 def encrypt(plaintext, table):
-    k = len(plaintext)
-    if k % 2 !=0:
-        plaintext = plaintext + "x"
     plaintext = plaintext.replace(" ", "")
     plaintext = plaintext.upper()
     ciphertext = ""
@@ -245,7 +272,7 @@ def encryptByPlayfairCipher(str, key):
 def decrypt(ciphertext, table):
     ciphertext = ciphertext.upper()
     plaintext = ""
-
+    
     for i in range(0, len(ciphertext)-1, 2):
         c1 = ciphertext[i]
         c2 = ciphertext[i+1]
@@ -287,6 +314,7 @@ def encrypt_vigenere(plaintext, key, vigenere_dict,dict2):
     while i < len(plaintext):
         if  not plaintext[i].isalpha():
             cyp = cyp + plaintext[i]
+            i = i + 1
             continue
         # print(alphabet_dict[plaintext[i]])
         # print(alphabet_dict[key[i]])
@@ -310,7 +338,8 @@ def decrypt_vigenere(cyphertext , key, vigenere_dict,dict2):
     i = 0
     while i < len(cyphertext):
         if  not cyphertext[i].isalpha():
-            cyp = cyp + cyphertext[i]
+            dec = dec + cyphertext[i]
+            i =  i + 1
             continue
         # print(alphabet_dict[cypher_text[i]])
         # print(alphabet_dict[key[i]])
@@ -321,8 +350,77 @@ def decrypt_vigenere(cyphertext , key, vigenere_dict,dict2):
     
     return dec
 
+def check_word_2(plaintext):
+    k = len(plaintext) %2
+    if k!=0:
+        for i in range(k):
+            plaintext = plaintext + 'x'
+    return plaintext 
+
+def encrypt_hill_2(plaintext, key_matrix, alphabet_dict,dict22):
+    plaintext = check_word_2(plaintext)
+    p = [[0],[0]]
+    cypher = "" 
+    for i in range(0,len(plaintext)-1,2):
+        if  not plaintext[i].isalpha():
+            cypher = cypher + plaintext[i]
+            continue
+        else:
+            entry1 = alphabet_dict[plaintext[i]]
+            entry2 = alphabet_dict[plaintext[i+1]]
+            p[0][0] = entry1
+            p[1][0] = entry2
+            result = np.dot(key_matrix, p)
+            for j in range(2):
+                result[j][0] = result[j][0] % 26
+            for j in range(2):
+                cypher = cypher + dict22[result[j][0]]
+    
+    return cypher
+
+def decrypt_hill_2(ciphertext, key_matrix, alphabet_dict1,dict2):
+    ciphertext = check_word_2(ciphertext)
+    det = np.linalg.det(key_matrix)
+    det = int(det)
+    det = det % 26
+    plaintext = ""
+    p = [[0],[0]]
+
+    q = extended_euclidean(26,det)
+    a = key_matrix[0][0]
+    b = key_matrix[0][1]
+    c = key_matrix[1][0]
+    d = key_matrix[1][1]
+    inverse = np.array([[d, -b],
+                        [-c, a]]) 
+    inverse2 = inverse * q
+    inverse2 = inverse2 % 26
+    # for row in inverse2:
+    #     for element in row:
+    #         print(element, end=' ')
+    #     print()
+    for i in range(0,len(ciphertext)-1,2):
+        if  not ciphertext[i].isalpha():
+            plaintext = plaintext + ciphertext[i]
+            continue
+        else:
+            entry1 = alphabet_dict1[ciphertext[i]]
+            entry2 = alphabet_dict1[ciphertext[i+1]]
+            p[0][0] = entry1
+            p[1][0] = entry2
+            result = np.dot(inverse2, p)
+            # for row in result:
+            #     for element in row:
+            #         print(element, end=' ')
+            #     print()
+            for j in range(2):
+                result[j][0] = result[j][0] % 26
+            for j in range(2):
+                plaintext = plaintext + dict22[result[j][0]]
+    return plaintext
+
 # Hill cipher functions
-def check_word(k):
+def check_word_3(k):
     p = len(k) % 3
     if len(k) % 3 != 0:
         for i in range(p):
@@ -331,8 +429,8 @@ def check_word(k):
         #check_word(k)
     return k
 
-def encrypt_hill(plaintext, k,  alphabet_dict,dict22):
-    plaintext = check_word(plaintext)
+def encrypt_hill_3(plaintext, k,  alphabet_dict,dict2):
+    plaintext = check_word_3(plaintext)
     print(plaintext)
     p = [[0],[0],[0]]
     cypher = "" 
@@ -358,6 +456,39 @@ def encrypt_hill(plaintext, k,  alphabet_dict,dict22):
     
     return cypher
 
+def decrypt_hill_3(ciphertext, key,alphabet,dict2 ):
+    """
+    Decrypts ciphertext using the Hill cipher with a 3x3 matrix key.
+    :param ciphertext: string to decrypt
+    :param key: 3x3 matrix as a list of lists
+    :return: decrypted string
+    """
+    # Convert the key into a matrix and find its inverse modulo 26
+    key_matrix = Matrix(key)
+    
+    try:
+        # Attempt to compute the inverse
+        inverse_matrix = key_matrix.inv()
+        
+    except NonInvertibleMatrixError:
+        # Matrix is not invertible
+        return "Matrix is not invertible!"
+    key_matrix_inv = key_matrix.inv_mod(len(alphabet))
+    
+    # Convert ciphertext to numerical values
+    ciphertext_numbers = [alphabet.index(char) for char in ciphertext.upper()]
+
+    # Split the ciphertext into groups of 3
+    ciphertext_matrix = [ciphertext_numbers[i:i + 3] for i in range(0, len(ciphertext_numbers), 3)]
+
+    # Decrypt the ciphertext
+    decrypted_matrix = [key_matrix_inv * Matrix(group) % len(alphabet) for group in ciphertext_matrix]
+
+    # Convert the numerical values back to letters
+    decrypted_text = ''.join(''.join(alphabet[num] for num in group) for group in decrypted_matrix)
+    print(decrypted_text)
+
+    return decrypted_text
 # Generate inverse key matrix 
 
 def get_inverse_key(key):
@@ -372,32 +503,8 @@ def Inverse_mod26(det):
         if (det * i) % 26 == 1:
             return i
     return -1
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def decrypt_hill(ciphertext, key, alphabet, dict22):
-    """
-    Decrypts ciphertext using the Hill cipher with a 3x3 matrix key.
-    :param ciphertext: string to decrypt
-    :param key: 3x3 matrix as a list of lists
-    :return: decrypted string
-    """
-    # Convert the key into a matrix and find its inverse modulo 26
-    key_matrix = Matrix(key)
-    key_matrix_inv = key_matrix.inv_mod(len(alphabet))
 
-    # Convert ciphertext to numerical values
-    ciphertext_numbers = [alphabet.index(char) for char in ciphertext.upper()]
-
-    # Split the ciphertext into groups of 3
-    ciphertext_matrix = [ciphertext_numbers[i:i + 3] for i in range(0, len(ciphertext_numbers), 3)]
-
-    # Decrypt the ciphertext
-    decrypted_matrix = [key_matrix_inv * Matrix(group) % len(alphabet) for group in ciphertext_matrix]
-
-    # Convert the numerical values back to letters
-    decrypted_text = ''.join(''.join(alphabet[num] for num in group) for group in decrypted_matrix)
-
-    return decrypted_text
 
 # Monoalphabetic cipher functions
 def encrypt_monoalphabetic(plaintext, key, monoalpha_dict):
@@ -482,7 +589,9 @@ def decrypt_affine(ciphertext, a, b, alphabet):
     dec1 = 0
     m = 26
     X = extended_euclidean(m,a)
-
+    if X == None:
+        return "no multiplicative inverse"
+    
     for i in range(len(ciphertext)):
         if  not ciphertext[i].isalpha():
             dec = dec + ciphertext[i]
@@ -499,23 +608,22 @@ def decrypt_affine(ciphertext, a, b, alphabet):
     return dec
 
 def decrypt_affine2(ciphertext, a, b, alphabet):
-    # Count the frequency of each letter in the ciphertext (excluding spaces)
-    letter_counts = Counter(char.lower() for char in ciphertext if char.isalpha())
-
-    # Get the most common letters in the ciphertext
-    most_frequent_letters = [char for char, _ in letter_counts.most_common()]
-
-    # Create a mapping from the most frequent letters to "etaoinsrhdlucmfywgpbvkjxqz"
-    decrypt_mapping = dict(zip(most_frequent_letters, "etaoinsrhdlucmfywgpbvkjxqz"))
-    print (most_frequent_letters)
-
-    # Decrypt the text using the mapping, preserving spaces
-    decrypted_text = ""
-    for char in ciphertext:
-        if char.isalpha():
-            decrypted_text += decrypt_mapping.get(char.lower(), char.lower())
+    d = {}
+    for elem in ciphertext:
+        if elem not in d:
+            d[elem] = 1
         else:
-            decrypted_text += char
+            d[elem] += 1
+
+    filtered_keys = [key for key, value in d.items() if value > 1]
+    sorted_keys = sorted(filtered_keys, key=d.get, reverse=True)
+    letter_frequencies = ['e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'f', 'w', 'y', 'g', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z']
+
+    decrypt_mapping = dict(zip(filtered_keys, letter_frequencies))
+
+    decrypted_text = ""
+    for elem in ciphertext:
+        decrypted_text += decrypt_mapping.get(elem, elem)
 
     return decrypted_text
 
